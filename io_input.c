6d307e2f55ad6de6161829d04abc1819c7934b46
Alex Hagen <ahagen@purdue.edu>
2016-02-04 21:05:28 -0500

    first attempt at adc[1]

--- io_input.c
+++ io_input.c
@@ -1,19 +1,3 @@
-/*! \file io_input.c
-\brief Example: print digital and analog inputs.
-
-This file contains an example on how to use libpruio to print out the
-state of the digital GPIOs and the analog input lines.
-
-Licence: GPLv3
-
-Copyright 2014 by Thomas{ dOt ]Freiherr[ At ]gmx[ DoT }net
-
-
-Compile by: `gcc -Wall -o io_input io_input.c -lpruio`
-
-*/
-
-
 //! Message for the compiler.
 #define _GNU_SOURCE 1
 #include "stdio.h"
@@ -23,65 +7,53 @@ Compile by: `gcc -Wall -o io_input io_input.c -lpruio`
 #include <sys/types.h>
 #include <sys/time.h>
 #include "../c_wrapper/pruio.h"
+#include <sys/time.h>
 
-
-/*! \brief Wait for keystroke or timeout.
-\param mseconds Timeout value in milliseconds.
-\returns 0 if timeout, 1 if input available, -1 on error.
-
-Wait for a keystroke or timeout and return which of the events happened.
-
-*/
-int
-isleep(unsigned int mseconds)
-{
-  fd_set set;
-  struct timeval timeout;
-
-  /* Initialize the file descriptor set. */
-  FD_ZERO(&set);
-  FD_SET(STDIN_FILENO, &set);
-
-  /* Initialize the timeout data structure. */
-  timeout.tv_sec = 0;
-  timeout.tv_usec = mseconds * 1000;
-
-  return TEMP_FAILURE_RETRY(select(FD_SETSIZE,
-                                   &set, NULL, NULL,
-                                   &timeout));
-}
-
-//! The main function.
 int main(int argc, char **argv)
 {
+  struct timeval tv1, tv2, tv;
   pruIo *io = pruio_new(PRUIO_DEF_ACTIVE, 0x98, 0, 1); //! create new driver structure
-  do { //                                      pseudo loop to avoid goto
-    if (io->Errr) {
-               printf("initialisation failed (%s)\n", io->Errr); break;}
+  if (io->Errr) {
+    printf("initialisation failed (%s)\n", io->Errr);
+    return 0;
+  }
 
-    if (pruio_config(io, 1, 0x1FE, 0, 4)) {
-                       printf("config failed (%s)\n", io->Errr); break;}
+  if (pruio_config(io, 1, 0x1FE, 0, 4)) {
+    printf("config failed (%s)\n", io->Errr);
+    return 0;
+  }
 
-    struct termios oldt, newt; //             make terminal non-blocking
-    tcgetattr( STDIN_FILENO, &oldt );
-    newt = oldt;
-    newt.c_lflag &= ~( ICANON | ECHO );
-    newt.c_cc[VMIN] = 0;
-    newt.c_cc[VTIME] = 0;
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  int a;
+  int numreps = 1000;
+  gettimeofday(&tv1, NULL);
+  for (int i = 0; i < numreps; i++){
+    a = io->Adc->Value[0];
+  }
+  gettimeofday(&tv2, NULL);
 
-    while(!isleep(1)) { //                      run loop until keystroke
-      printf("\r%8X %8X %8X %8X  %4d %4X %4X %4X %4X %4X %4X %4X"
-           , io->Gpio->Raw[0]->Mix, io->Gpio->Raw[1]->Mix, io->Gpio->Raw[2]->Mix, io->Gpio->Raw[3]->Mix
-           , io->Adc->Value[1], io->Adc->Value[2], io->Adc->Value[3], io->Adc->Value[4]
-           , io->Adc->Value[5], io->Adc->Value[6], io->Adc->Value[7], io->Adc->Value[8]);
-      fflush(STDIN_FILENO);
-    }
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt); //           reset terminal
+  uint32_t t1 = 1000000 * tv1.tv_sec + tv1.tv_usec;
+  uint32_t t2 = 1000000 * tv2.tv_sec + tv2.tv_usec;
+  int numsamples = BUFLEN / 5;
+  printf("rate: %f kSps\n", numreps * 1.0E3
+      / static_cast<float>(t2 - t1));
 
-    printf("\n");
-  } while (0);
+  /*
+  struct termios oldt, newt; //             make terminal non-blocking
+  tcgetattr( STDIN_FILENO, &oldt );
+  newt = oldt;
+  newt.c_lflag &= ~( ICANON | ECHO );
+  newt.c_cc[VMIN] = 0;
+  newt.c_cc[VTIME] = 0;
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  */
+  /*
+  printf("\r%8X %8X %8X %8X  %4d %4X %4X %4X %4X %4X %4X %4X"
+       , io->Gpio->Raw[0]->Mix, io->Gpio->Raw[1]->Mix, io->Gpio->Raw[2]->Mix, io->Gpio->Raw[3]->Mix
+       , io->Adc->Value[1], io->Adc->Value[2], io->Adc->Value[3], io->Adc->Value[4]
+       , io->Adc->Value[5], io->Adc->Value[6], io->Adc->Value[7], io->Adc->Value[8]);
 
+  //tcsetattr(STDIN_FILENO, TCSANOW, &oldt); //           reset terminal
+  */
   pruio_destroy(io);       /* destroy driver structure */
 	return 0;
 }
